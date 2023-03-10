ef0b8c44d8eed4a940831498e7bc74db4a743056
AntonKulagin <i-gorusich@mail.ru>
2023-03-10 20:18:47 +0300

    created hook useHover

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import HookLocalStorageComponent from './components/hooksComponents/hookLocalStorageComponent';
 import HookToggleComponent from './components/hooksComponents/hookToggleComponent';
+import { useHover } from './hooks/useHover';
 
 function App() {
+  const { hovered, ref } = useHover();
   return (
     <div className="App">
       <header className="App-header">
         <HookToggleComponent />
         <HookLocalStorageComponent />
+        <div ref={ref}>{hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}</div>
       </header>
     </div>
   );
