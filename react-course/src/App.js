775ba3f93b46db6e395f177b5315b49909408468
AntonKulagin <i-gorusich@mail.ru>
2023-03-10 20:33:12 +0300

    created hook useViewportSize

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { useViewportSize } from './hooks/useViewportSize';
 
 function App() {
+  const { height, width } = useViewportSize();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Width: {width}, height: {height}
       </header>
     </div>
   );
