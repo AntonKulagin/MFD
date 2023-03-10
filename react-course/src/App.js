95fdb1b61784cef95b3b2dd5c9fa411a4032506a
AntonKulagin <i-gorusich@mail.ru>
2023-03-10 20:18:47 +0300

    created hook useHover

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { useHover } from './hooks/useHover';
 
 function App() {
+  const { hovered, ref } = useHover();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div ref={ref}>{hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}</div>
       </header>
     </div>
   );
