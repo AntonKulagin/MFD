d489d199c14d84588f169719fae87ea78a32069c
AntonKulagin <i-gorusich@mail.ru>
2023-03-10 20:59:15 +0300

    created more difficult hook useToggle

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { useToggle } from './hooks/useToggle';
 
 function App() {
+  const [value, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal']);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => toggle()}>{value}</button>
       </header>
     </div>
   );
---
+++ react-course/src/hooks/useToggle.js
@@ -0,0 +1,62 @@
+import { useReducer } from 'react';
+
+const getInitialState = (data) => {
+  if (Array.isArray(data)) {
+    return {
+      data,
+      count: 0,
+    };
+  }
+  return {
+    data: [false, true],
+    count: 0,
+  };
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'item':
+      return {
+        ...state,
+        data: state.data.filter((item) => item === action.payload),
+      };
+    case 'booleanItem':
+      return {
+        ...state,
+        data: [action.payload],
+      };
+    case 'loop':
+      return {
+        ...state,
+        count: state.count >= state.data.length - 1 ? 0 : state.count + 1,
+      };
+    default:
+      break;
+  }
+};
+
+export function useToggle(data) {
+  const [state, dispatch] = useReducer(reducer, getInitialState(data));
+
+  const toggle = (item) => {
+    if (typeof item === 'string') {
+      dispatch({
+        type: 'item',
+        payload: item,
+      });
+    }
+    if (typeof item === 'boolean') {
+      dispatch({
+        type: 'booleanItem',
+        payload: item,
+      });
+    }
+    dispatch({
+      type: 'loop',
+    });
+  };
+
+  const value = state.data[state.count].toString();
+
+  return [value, toggle];
+}
