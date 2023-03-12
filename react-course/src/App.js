27c898b1226e764ddde9c1e860a466385ddf8f25
AntonKulagin <i-gorusich@mail.ru>
2023-03-12 19:27:48 +0300

    added WindowScroll to react

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HookLocalStorageComponent from './components/hooksComponents/hookLocalStorageComponent';
 import HookToggleComponent from './components/hooksComponents/hookToggleComponent';
 import HookHoverComponent from './components/hooksComponents/hookHoverComponent';
+import HookWindowScrollComponent from './components/hooksComponents/hookWindowScrollComponent';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <HookToggleComponent />
         <HookLocalStorageComponent />
         <HookHoverComponent />
+        <HookWindowScrollComponent />
       </header>
     </div>
   );
