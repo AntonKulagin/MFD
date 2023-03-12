206d26bea02ca3389b1c553491fd7536b229552e
AntonKulagin <i-gorusich@mail.ru>
2023-03-12 18:07:24 +0300

    display corrected hook hover

--- react-course/src/components/hooksComponents/hookHoverComponent.jsx
+++ react-course/src/components/hooksComponents/hookHoverComponent.jsx
@@ -3,7 +3,13 @@ import { useHover } from '../../hooks/useHover';
 
 const HookHoverComponent = () => {
   const { hovered, ref } = useHover();
-  return <div ref={ref}>{hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}</div>;
+  return (
+    <div>
+      <hr style={{ width: '90vw' }} />
+      <h3>Hook Hover</h3>
+      <div ref={ref}>{hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}</div>;
+    </div>
+  );
 };
 
 export default HookHoverComponent;
