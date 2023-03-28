b596cc6f763ca9df63d6842ef805637f2bed77c6
AntonKulagin <i-gorusich@mail.ru>
2023-03-28 23:11:57 +0300

    fix button styles

--- react-course/src/components/hooksComponents/hookFetch.jsx
+++ react-course/src/components/hooksComponents/hookFetch.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import { useFetch } from '../../hooks/useFetch'
+import { MyButton } from '../UI/myButton/myButton'
+import { TaskBlock } from '../UI/taskBlock'
 
 export const HookFetch = () => {
   const { data, isLoading, error, refetch } = useFetch(
     'https://jsonplaceholder.typicode.com/posts',
   )
   return (
-    <div>
-      {' '}
+    <TaskBlock>
+      <h3>Hook Fetch</h3>
       <div>
-        <button onClick={() => refetch({ params: { _limit: 3 } })}>
+        <MyButton onClick={() => refetch({ params: { _limit: 3 } })}>
           Перезапросить
-        </button>
+        </MyButton>
       </div>
       {isLoading && <span style={{ color: 'greenyellow' }}>Загрузка...</span>}
       {error && <span style={{ color: 'red' }}>Произошла ошибка</span>}
       {data &&
         !isLoading &&
         data.map((item) => <div key={item.id}>{item.title}</div>)}
-    </div>
+    </TaskBlock>
   )
 }
